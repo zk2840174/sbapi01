2c6f7f0404eef3d62d613c4e82b7892fdc5aaecb
zk2840174 <zk2840174@gmail.com>
2023-02-14 16:45:10 +0900

    Initial commit

---
+++ src/main/java/org/zerock/sbapi01/Sbapi01Application.java
@@ -0,0 +1,13 @@
+package org.zerock.sbapi01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbapi01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sbapi01Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/zerock/sbapi01/Sbapi01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.sbapi01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbapi01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
