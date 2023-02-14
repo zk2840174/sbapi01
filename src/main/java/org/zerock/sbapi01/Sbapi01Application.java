87a30083f2e119bfdc0264985af14e0e1e8863d0
zk2840174 <zk2840174@gmail.com>
2023-02-14 17:53:19 +0900

    DB config

--- src/main/java/org/zerock/sbapi01/Sbapi01Application.java
+++ src/main/java/org/zerock/sbapi01/Sbapi01Application.java
@@ -3,11 +3,16 @@ package org.zerock.sbapi01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.Instant;
+import java.util.Date;
+
 @SpringBootApplication
 public class Sbapi01Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Sbapi01Application.class, args);
+
     }
 
 }
